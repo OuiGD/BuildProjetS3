67f3463fa3e3e421e05383919e26c2186f8650d1
Tim <73876946+Etienn124@users.noreply.github.com>
2021-11-28 22:10:08 +0100

    UI
    UI faite, excepté le carroussel

--- Assets/Script/bouton_menu.cs
+++ Assets/Script/bouton_menu.cs
@@ -7,7 +7,42 @@ public class bouton_menu : MonoBehaviour
 {
     public void OnbuttonPress()
     {
-        Debug.Log("test");
-        SceneManager.LoadScene("test");
+        //renvoie au selecteur de mission
+        SceneManager.LoadScene("Selecteur mission");
+    }
+
+    public void Ouverture_contrat()
+    {
+        //déploie le contrat
+        SceneManager.LoadScene("Selecteur mission with contrat");
+    }
+
+    public void Contrat_return()
+    {
+        //renvoie au selecteur de mission
+        SceneManager.LoadScene("Selecteur mission");
+    }
+
+    public void Contrat_continue()
+    {
+        //renvoie au hangar de construction
+        SceneManager.LoadScene("Hanger_contruction");
+    }
+
+    public void Hangar_no_contrat()
+    {
+        //ne lance pas de mission quand il renvoie au hangar
+        SceneManager.LoadScene("Hanger_contruction");
+    }
+
+    public void Menu()
+    {
+        //renvoie au menu
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+    public void validation()
+    {
+        //pour l'instant, ce boutton ne fais rien, mais il servira à valider les missions
     }
 }
