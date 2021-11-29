2826a038bb16d0c9434d9790e334090d66e90d33
Tim <73876946+Etienn124@users.noreply.github.com>
2021-11-30 00:04:31 +0100

    final V1
    bug and fixes pour le premier prototype

--- Assets/Script/GyroControler.cs
+++ Assets/Script/GyroControler.cs
@@ -24,8 +24,6 @@ public class GyroControler : MonoBehaviour
             gyro = Input.gyro;
             gyro.enabled = true;
 
-            rot = new Quaternion(0, 0, 1, 0);
-
             return true;
         }
         return false;
@@ -36,7 +34,7 @@ public class GyroControler : MonoBehaviour
     {       
         if (gyroEnabled)
         {
-            target.transform.localRotation = gyro.attitude * rot;
+            target.transform.localRotation = gyro.attitude;
         }
         else
         {
@@ -45,45 +43,3 @@ public class GyroControler : MonoBehaviour
     }
 }
 
-/*
-
-public class GyroControl : MonoBehaviour
-    {
-        private bool gyroEnabled;
-        private Gyroscope gyro;
-
-        private GameObject cameraContainer;
-        private Quaternion rot;
-
-        private void Start()
-        {
-            cameraContainer = new GameObject("Camera Container");
-            cameraContainer.transform.position = transform.position;
-            transform.SetParent(cameraContainer.transform);
-
-            gyroEnabled = EnableGyro();
-        }
-
-        private bool EnableGyro()
-        {
-            if (SystemInfo.supportsGyroscope)
-            {
-                gyro = Input.gyro;
-                gyro.enabled = true;
-
-                cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
-                rot = new Quaternion(0, 0, 1, 0);
-
-                return true;
-            }
-            return false;
-        }
-        private void Update()
-        {
-            if (gyroEnabled)
-            {
-                transform.localRotation = gyro.attitude * rot;
-            }
-        }
-    }
-*/
