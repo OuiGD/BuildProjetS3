7fc6102ca22606c8862d5eaa9b734b9624e52d73
OuiGD <73877098+OuiGD@users.noreply.github.com>
2021-12-10 14:28:14 +0100

    muchos dineros

--- Assets/Script/GUI.cs
+++ Assets/Script/GUI.cs
@@ -28,9 +28,9 @@ public class GUI : MonoBehaviour
     const float FACTOR_PUISSANCE = 0.10f;
     const float FACTOR_PORTANCE = 0.25f;
 
-    public float POID_MAX = 100;
-    public float CHARGE_MAX = 100;
-    public float VITESSE_MAX = 100;
+    public float POID_MAX;
+    public float CHARGE_MAX;
+    public float VITESSE_MAX;
 
 
     // Start is called before the first frame update
@@ -44,9 +44,18 @@ public class GUI : MonoBehaviour
         Portance = 0f;
         Puissance = 0f;
 
+        POID_MAX = Mission.Objectif_poid;
+        CHARGE_MAX = Mission.Objectif_Charge;
+        VITESSE_MAX = Mission.Objectif_Vitesse;
+
         statBarPoid.fillAmount = Poid;
         statBarCharge.fillAmount = Charge;
         statBarVitesse.fillAmount = Vitesse;
+
+        Debug.Log("poid max: "+POID_MAX);
+        Debug.Log("charge max: "+CHARGE_MAX);
+        Debug.Log("vitesse max: "+VITESSE_MAX);
+        Debug.Log("************************");
     }
 
     public void SelectionWheel(float x, float Axis)
@@ -59,9 +68,21 @@ public class GUI : MonoBehaviour
         Puissance += (float)objAjout.Puissance;
         Portance += (float)objAjout.Portance;
         Poid += (float)objAjout.Poid;
+
+        Debug.Log("puissance actuel: " + Puissance);
+        Debug.Log("portance actuel: " + Portance);
+        Debug.Log("poid actuel: " + Poid);
+        Debug.Log("************************");
+
+        Poid = Poid * 0.1f;
         Vitesse = (Puissance - Poid) * (Portance * Mathf.Pow(FACTOR_PORTANCE, 2));
         Charge = (Portance * FACTOR_PORTANCE) + (Puissance * FACTOR_PUISSANCE) - (Poid * FACTOR_POID);
-        statBarPoid.fillAmount = Poid/ POID_MAX;
+
+        Debug.Log("vitesse actuel: " + Vitesse);
+        Debug.Log("charge actuel: " + Charge);
+        Debug.Log("************************");
+
+        statBarPoid.fillAmount = Poid / (POID_MAX*0.1f);
         statBarVitesse.fillAmount = Vitesse / VITESSE_MAX;
         statBarCharge.fillAmount = Charge / CHARGE_MAX;
     }
